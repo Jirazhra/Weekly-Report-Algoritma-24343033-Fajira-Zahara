6847a9caacfb22b36b1f3aacc7dfb79992311e7c
fajira zahara <jirazahra@student.unp.ac.id>
2024-12-01 22:05:18 +0700

    Add files via upload

---
+++ Modul 5/TUGAS KALKULATOR SEDERHANA.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+    float angka_pertama, angka_kedua, hasil;
+    int pilihan;
+
+    // main menu
+    printf("===kalkulator sederhana===\n");
+    printf("a.penjumlahan\n");
+    printf("b.pengurangan\n");
+    printf("c.perkalian\n");
+    printf("d.pembagian\n");
+    printf("e.hasil bagi\n");
+    printf("pilih operaasi (a-e): ");
+    scanf("%d", &pilihan);
+
+    // meminta input angka
+    printf("masukkan angka pertama: ");
+    scanf("%f", angka_pertama);
+    printf("masukkan angka kedua: ");
+    scanf("%f", angka_kedua);
+
+    switch (pilihan)
+    {
+    case 1: // penjumlahan
+        hasil = angka_pertama + angka_kedua;
+        printf("Hasil Penjumlahan: %.2f\n", hasil);
+        break;
+    case 2: // Pengurangan
+        hasil = angka_pertama - angka_kedua;
+        printf("Hasil Pengurangan: %.2f\n", hasil);
+        break;
+    case 3: // Perkalian
+        hasil = angka_pertama * angka_kedua;
+        printf("Hasil Perkalian: %.2f\n", hasil);
+        break;
+    case 4: // Pembagian
+        if (angka_kedua != 0)
+        {
+            hasil = angka_pertama / angka_kedua;
+            printf("Hasil Pembagian: %.2f\n", hasil);
+        }
+        else
+        {
+            printf("Error: Pembagian dengan nol!\n");
+        }
+        break;
+    case 5: // Hasil Bagi (Pembagian Bulat)
+        if (angka_kedua != 0)
+        {
+            int hasil_bagi = (int)angka_pertama / (int)angka_kedua; // Pembagian bulat
+            printf("Hasil Bagi: %d\n", hasil_bagi);
+        }
+        else
+        {
+            printf("Error: Pembagian dengan nol!\n");
+        }
+        break;
+    default:
+        printf("pilihan tidak valid!\n");
+        break;
+    }
+
+    return 0;
+}
